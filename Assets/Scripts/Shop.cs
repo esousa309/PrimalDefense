4e780af6ceea632a05458468b8b854a77c0e92c4
esousa309 <esousa@309technology.com>
2025-11-07 19:42:10 +0000

    fix 1

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public TurretBlueprint standardTurret;
+    public TurretBlueprint anotherTurret;
+
+    BuildManager buildManager;
+
+    void Start()
+    {
+        buildManager = BuildManager.instance;
+    }
+
+    public void SelectStandardTurret()
+    {
+        Debug.Log("Standard Turret Selected");
+        buildManager.SelectTurretToBuild(standardTurret);
+    }
+
+    public void SelectAnotherTurret()
+    {
+        Debug.Log("Another Turret Selected");
+        buildManager.SelectTurretToBuild(anotherTurret);
+    }
+}
