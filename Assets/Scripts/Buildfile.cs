fcedbebceff4d2e7f13fb7b4911729d3d9ec206b
esousa309 <esousa@309technology.com>
2025-11-07 16:03:02 +0000

    attempt button fix 2

--- Assets/Scripts/Buildfile.cs
+++ Assets/Scripts/Buildfile.cs
@@ -2,11 +2,5 @@ using UnityEngine;
 
 public class BuildTile : MonoBehaviour
 {
-    // This is a special Unity function that is called when a mouse clicks on any 3D object with a collider.
-    void OnMouseDown()
-    {
-        // When this tile is clicked, we access the global BuildManager instance...
-        // ...and we call its function, passing in our own transform (position, rotation, scale).
-        BuildManager.instance.BuildTowerOnTile(transform);
-    }
+
 }
